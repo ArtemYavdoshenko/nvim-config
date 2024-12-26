97f5cbba4db9a3146705907513b360a18eb6c3ed
ArtemYavdoshenko <artem.yavd@gmail.com>
2024-12-26 13:57:15 +0100

    minor fixes

--- init.lua
+++ init.lua
@@ -983,5 +983,8 @@ require('lazy').setup({
   },
 })
 
+-- Set language to English:
+vim.cmd 'language en_US'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
