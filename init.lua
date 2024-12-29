40350e34dc3e3580edb7fe3390962ea77d21ec08
= <artem.yavd@gmail.com>
2024-12-29 14:08:48 +0100

    changed neovim and lualine themes

--- init.lua
+++ init.lua
@@ -866,7 +866,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'sorbet'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -900,6 +900,9 @@ require('lazy').setup({
       local statusline = require 'lualine'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
+      require('lualine').setup {
+        options = { theme = 'palenight' },
+      }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
